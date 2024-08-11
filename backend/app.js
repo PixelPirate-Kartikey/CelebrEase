91dcbb1c21d3d9a5fe8559ae9f6fecbef5f8bd47
Kartikey Saxena <104156929+PixelPirate-Kartikey@users.noreply.github.com>
2024-08-11 08:54:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,20 @@ const app = express();
 
 dotenv.config({ path: "./config/config.env" });
 
+// CORS configuration
 app.use(
   cors({
     origin: ["https://celebrease.vercel.app", "https://celebrease.onrender.com"],
-    methods: ["POST"],
+    methods: ["GET", "POST", "OPTIONS"], // Add OPTIONS method for preflight requests
     credentials: true,
   })
 );
 
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Test route to check if the server is running
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -26,8 +29,10 @@ app.get("/", (req, res) => {
   });
 });
 
+// Message routes
 app.use("/api/v1/message", messageRouter);
 
+// Database connection
 dbConnection();
 
 export default app;
